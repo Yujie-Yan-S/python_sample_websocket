f9638175b94dcbc156bb3424bea463341bff8925
zgong26 <79713887+zgong26@users.noreply.github.com>
2022-12-02 15:16:24 -0500

    Update test1.py

--- src/test1.py
+++ src/test1.py
@@ -9,38 +9,47 @@ from socket import *
 # task queue
 tasks = []
 
-sockets = []
+sockets = [None] * 3
 sockets[0] = socket(AF_INET, SOCK_STREAM)
-sockets[0].connect((IP_Address, 12345))
+sockets[0].connect(("192.122.236.104", 12345))
 
 sockets[1] = socket(AF_INET, SOCK_STREAM)
-sockets[1].connect((IP_Address, 12345))
+sockets[1].connect(("164.67.126.43", 12345))
 
 sockets[2] = socket(AF_INET, SOCK_STREAM)
-sockets[2].connect((IP_Address, 12345))
+sockets[2].connect(("143.215.216.194", 12345))
+
+clients = [None] * 3
+available_machine = [True, True, True]
 
 
-def message_received(client, server, message):
+def message_received(client, message):
     # 接收到了uri
     # 储存到tasks queue里
-    tasks.append(message)
-    # server.send_message(client,message)
-    # 处理返回图片的分类数据
+    tasks.append((client, message))
 
 
-def message_handler():
-    available_machine = [True, True, True]
-    # 从queue中取出下一个task交给后端
+def load_balance():
+    # 从queue中取出下一个task交给后端一台available machine上
     while True:
         if True in available_machine and len(tasks) != 0:
             index = available_machine.index(True)
+            available_machine[index] = False  # 锁死
             task = tasks.pop(0)
-            available_machine[index] = False
-            threading.Thread(target=send_task, args=(sockets[index], task)).start()
+            client = task[0]  # 获取client
+            message = task[1] + "\n"  # 获取url
+            clients[index] = client  # 储存client信息
+            threading.Thread(target=task_handler, args=(index, message)).start()  # 处理信息
 
 
-def send_task(s, message):
-    s.send(message.encode())
+def task_handler(index, message):
+    s = sockets[index]
+    s.send(message.encode())  # 发送url
+    res = s.recv(4096)  # 接受结果
+    client = clients[index]
+    clients[index] = None
+    available_machine[index] = True  # 解锁
+    server.send_message(client, res)  # 向前端返回结果
 
 
 if __name__ == '__main__':
@@ -51,5 +60,5 @@ if __name__ == '__main__':
     server.set_fn_message_received(message_received)
     server_thread = threading.Thread(target=server.run_forever)
     server_thread.start()
-    threading.Thread(target=message_handler).start()
+    threading.Thread(target=load_balance).start()
     print("ok")
